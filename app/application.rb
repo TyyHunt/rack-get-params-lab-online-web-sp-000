3886fc2566934d238108b94e7f748877ab80d337
Tyler Hunt <tyhunt04@gmail.com>
2019-03-25 16:18:35 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -15,8 +15,7 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
-      search_term = req.params["q"]
-      resp.write handle_search(search_term)
+      show_cart
     else
       resp.write "Path Not Found"
     end
