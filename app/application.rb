523134762d9e538d8028cc1faf3eb8faf3dc30c2
Tyler Hunt <tyhunt04@gmail.com>
2019-03-25 16:26:00 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -26,6 +26,7 @@ class Application
       search_term = req.params["q"]
       if @@items.include?(search_term)
         @@cart << search_term
+        resp.write "add #{search_term}"
       else
         resp.write "We don't have that item"
       end
